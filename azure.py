a4de29b6281295da3bfa45b6ca29d22f2ecc5ccc
WCS19 <wcs@bu.edu>
2023-10-26 20:49:19 -0400

    adding comments

--- azure.py
+++ azure.py
@@ -7,9 +7,13 @@ from langchain.memory import ConversationBufferMemory
 from langchain.prompts.chat import SystemMessagePromptTemplate
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 
+#pull openAI key from Azure config
 openai_api_key=os.environ['OPENAI_API_KEY']
+
+#App Name
 st.title('TaxBot')
 
+# Initialize the chat model and memory
 chat = ChatOpenAI(openai_api_key=openai_api_key)
 memory = ConversationBufferMemory()
 
@@ -40,41 +44,34 @@ Please feel free to ask clarifying questions if more context is needed.
 # Use st.selectbox to let the user select a template
 selected_template = st.selectbox("Choose a business vertical to continue:", list(templates.keys()))
 
-# Set the system_message based on the user's selection
+# Set the system template based on the user's selection
 template = templates[selected_template]
+
+# Define prompt template to set contextual scope
 system_message = SystemMessagePromptTemplate.from_template(template=template)
 PROMPT = PromptTemplate(input_variables=['history', 'input'], template=template + '.\n\nCurrent conversation:\n{history}\nHuman: {input}\nAI:')
 
+# conversation = ConversationChain(
+#     llm=chat,
+#     prompt=PROMPT,
+#     verbose=False, 
+#     memory=memory,
+# )
+
+#Initialize conversation chain
 conversation = ConversationChain(
     llm=chat,
     prompt=PROMPT,
-    verbose=False, 
-    memory=memory,
-)
-# template =  '''
-# You are a Tax professional. 
-# You are talking to a junior accountant. 
-# Please answer as honestly as possible. 
-# Please feel free to ask clarifying questions if more context is needed. 
-# If asked to calculate anything, do not assume any numerical dollar values, ask for exact amounts.
-# '''
-
-# system_message = SystemMessagePromptTemplate.from_template(template=template)
-# PROMPT = PromptTemplate(input_variables=['history', 'input'], template=template + '.\n\nCurrent conversation:\n{history}\nHuman: {input}\nAI:')
-
-conversation = ConversationChain(
-    llm=chat,
-    prompt=PROMPT,
-    verbose=False, 
+    verbose=False, #set verbose to false to hide the conversation history on frontend 
     memory=memory,
 )
 
-# Initialize session state for conversation history if it doesn't exist
+# Initialize session state for conversation history if it doesn't already exist
 if 'conversation_history' not in st.session_state:
     st.session_state.conversation_history = []
 
-# Always display the text input box
-user_input = st.text_input("Please enter your message:")
+#Display textbox outside of button send logic
+user_input = st.text_input("Please enter your question:")
 
 if st.button('Send'):
     if user_input:
@@ -89,27 +86,3 @@ if st.button('Send'):
         
         # Display the AI's response
         st.write(response)
-
-#2nd iteration of code                  didnt capture conversation history
-# Always display the text input box
-# user_input = st.text_input("Please enter your message:")
-
-# if st.button('Send'):
-#     if user_input:
-#         response = conversation.predict(input=user_input)
-#         st.write(response)
-
-
-
-
-
-
-#initial code, commenting out to test streamlit functionality
-# def chat_with_bot():
-#     user_input = st.text_input("Please enter your message:")
-#     if user_input:
-#         response = conversation.predict(input=user_input)
-#         st.write(response)
-
-# if st.button('Send'):
-#     chat_with_bot()
