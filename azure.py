0126daa196aaf43e950f3d7f92e2eccb3018b438
WCS19 <wcs@bu.edu>
2023-12-03 15:53:49 -0500

    bug fixes

--- azure.py
+++ azure.py
@@ -11,13 +11,25 @@ from langchain.prompts import ChatPromptTemplate, PromptTemplate
 
 
 #Hardcoded open api key for local testing 
-openai_api_key='sk-xxx'
+openai_api_key='sk-x'
+
 
 # Function to initialize the chat model and memory
 def initialize_chat_model(api_key, model_name):
     chat = ChatOpenAI(openai_api_key=api_key, model=model_name)
     memory = ConversationBufferMemory()
     return chat, memory
+
+# Function to get verbosity instruction
+def get_verbosity_instruction(verbosity):
+    if verbosity == 'Low':
+        return 'Please respond in a concise manner. Keep your answer short and to the point.'
+    elif verbosity == 'Medium':
+        return 'Please provide a balanced response with enough detail to be clear, but not overly verbose.'
+    elif verbosity == 'High':
+        return 'Feel free to provide detailed and comprehensive responses, elaborating as much as necessary.'
+    else:
+        return ''
 
 # Function to define multiple nested templates
 def get_templates():
@@ -46,10 +58,12 @@ def get_templates():
         '''
     }
 
+
 # Function to reset the conversation
 def reset_conversation():
     st.session_state.conversation_history = []
     initialize_chat_model(api_key=openai_api_key, model_name="gpt-4-1106-preview")
+
 
 # Function to handle file uploads
 def handle_file_upload():
@@ -64,14 +78,6 @@ def handle_file_upload():
             string_data = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
     return df, string_data
 
-# # Function to process and display user input and AI response
-# def process_user_input(conversation, df, string_data):
-#     user_input = st.text_input("Please enter your question:")
-#     if st.button('Send'):
-#         update_conversation_history(user_input, df, string_data)
-#         response = conversation.predict(input="\n".join(st.session_state.conversation_history))
-#         st.session_state.conversation_history.append(f"AI: {response}")
-#         st.write(response)
 
 # Modified process_user_input function
 def process_user_input(conversation, df, string_data):
@@ -93,23 +99,46 @@ def update_conversation_history(user_input, df=None, string_data=None):
     if df is not None and not df.empty:
         st.session_state.conversation_history.append(f"File Content: {df}")
     if user_input:
-        st.session_state.conversation_history.append(f"Human: {user_input}")
+        st.session_state.conversation_history.append(f"You: {user_input}")
 
+
+# Main Application
+
+#Pull openAI key from Azure config
+# openai_api_key=os.environ['OPENAI_API_KEY'] #uncomment to run on Azure
 
 # Main function to run the app
 def main():
     st.title('Accounting Assistant')
-    openai_api_key=openai_api_key
+    # Initialize the chat model and memory
+    openai_api_key = 'sk-x'
     chat, memory = initialize_chat_model(api_key=openai_api_key, model_name="gpt-4-1106-preview")
+    # Retrieve templates
     templates = get_templates()
+    # Add a slider for verbosity selection
+    verbosity = st.select_slider("Select response verbosity", 
+                                 options=['Low', 'Medium', 'High'],
+                                 value='Medium')  # Default value set to 'Medium'
+    # Get the verbosity instruction based on the selection
+    verbosity_instruction = get_verbosity_instruction(verbosity)
+    # User selects a business vertical
     selected_template = st.selectbox("Choose a business vertical to continue:", list(templates.keys()))
-    template = templates[selected_template]
-    system_message = SystemMessagePromptTemplate.from_template(template=template)
-    PROMPT = PromptTemplate(input_variables=['history', 'input'], template=template + '.\n\nCurrent conversation:\n{history}\nHuman: {input}\nAI:')
+    # Append the verbosity instruction to the selected template
+    template_with_verbosity = templates[selected_template] + "\n\n" + verbosity_instruction
+    # Set up the system message and prompt template
+    system_message = SystemMessagePromptTemplate.from_template(template=template_with_verbosity)
+    PROMPT = PromptTemplate(input_variables=['history', 'input'], template=template_with_verbosity + '.\n\nCurrent conversation:\n{history}\nHuman: {input}\nAI:')
+    # Create the conversation chain
     conversation = ConversationChain(llm=chat, prompt=PROMPT, verbose=False, memory=memory)
+    # Handle file upload
     df, string_data = handle_file_upload()
+    # Process user input
     process_user_input(conversation, df, string_data)
+    # Add a reset button
+    if st.button("Reset Conversation"):
+        reset_conversation()
 
+# Run the app
 if __name__ == "__main__":
     main()
 
